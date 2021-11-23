f34f402bb4fe8c62d84ef7d1fbb5e4b456ff00e6
Gabriel Greison <gabrielgsousa72@gmail.com>
2021-11-23 12:11:00 -0300

    counter system exercise

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
+const Compras = () => {
+  const [contador, setContador] = React.useState(0);
+  function comprar() {
+    setContador(contador + 1);
+  }
 
-    const Button = () => {
-        function handleClick(event){
-          console.log(event.target.innerText);
-        }
-        return <button onClick={handleClick}> Comprar </button>
-      }
-  
-      const Titulo = () => {
-        const numero = Math.random() * 100;
-        return <h1>Meu título {numero}</h1>
-      }
-  
-      const App = () => {
-        return <div>Meu App
-          <Titulo />
-          <Button />
-          <div>
-            Compre aqui também 
-            <Button />
-          </div>
-              </div>
-      }
-  
-      ReactDOM.render(<App />, document.getElementById('app'))
+  if (contador <= 10) {
+    return (
+      <div>
+        <button onClick={comprar}>Comprar</button>
+        <p>Total : {contador}</p>
+        <p>Preço : R$ {contador * 250}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <button onClick={comprar}>Comprar</button>
+        <p>Total : {contador}</p>
+        <p>Preço : R$ {contador * 175}</p>
+      </div>
+    );
+  }
+};
+
+const Button = () => {
+  function handleClick(event) {
+    console.log(event.target.innerText);
+  }
+  return <button onClick={handleClick}> Comprar </button>;
+};
+
+const Titulo = () => {
+  const numero = Math.random() * 100;
+  return <h1>Meu título {numero}</h1>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <h1>Carrinho de Compras</h1>
+      <h2>Preço Unitário: R$ 250,00  (Acima de 10 peças, 30% de desconto)</h2>
+      <Compras />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
