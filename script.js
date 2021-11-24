0353a7b22ff4a9f38779e9d95f1281ad2afe08fb
Gabriel Greison <gabrielgsousa72@gmail.com>
2021-11-24 10:20:01 -0300

    refactoring

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const Compras = () => {
   const [contador, setContador] = React.useState(0);
-  function comprar() {
-    setContador(contador + 1);
-  }
 
   if (contador <= 10) {
     return (
       <div>
-        <button onClick={comprar}>Comprar</button>
+        <button onClick={() => setContador(contador +1)}>Comprar</button>
         <p>Total : {contador}</p>
         <p>Preço : R$ {contador * 250}</p>
       </div>
@@ -15,24 +12,12 @@ const Compras = () => {
   } else {
     return (
       <div>
-        <button onClick={comprar}>Comprar</button>
+        <button onClick={() => setContador(contador +1)}>Comprar</button>
         <p>Total : {contador}</p>
         <p>Preço : R$ {contador * 175}</p>
       </div>
     );
   }
-};
-
-const Button = () => {
-  function handleClick(event) {
-    console.log(event.target.innerText);
-  }
-  return <button onClick={handleClick}> Comprar </button>;
-};
-
-const Titulo = () => {
-  const numero = Math.random() * 100;
-  return <h1>Meu título {numero}</h1>;
 };
 
 const App = () => {
